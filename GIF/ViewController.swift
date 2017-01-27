295b92deee722fb551f316d9b9708ac0f82dc768
Benjamin Lowry <lowryb1@rchk.edu.hk>
2017-01-27 18:02:50 +0800

    First draft commit

--- GIF/ViewController.swift
+++ GIF/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //our scroll view for viewing GIFs
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //to be used to store GIF file names
+    var GIFFileArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //initialize the array of GIF file names
+        //add the names of the GIFs you want to show
+        GIFFileArray = ["giphy", "giphy2"]
+        
+        //loop through each GIF file in the array
+        for GIFFileName in GIFFileArray {
+            
+            //create a new image view
+            let imageView = UIImageView()
+            //load GIF onto image view
+            imageView.loadGif(name: GIFFileName)
+            //make the GIF fit the width of the screen, without altering height
+            imageView.contentMode = .scaleAspectFit
+            
+            //find the index of the file name in the array
+            let index = CGFloat(GIFFileArray.index(of: GIFFileName)!)
+            
+            //find the x-coordinate that the GIF will be at in the scrollView
+            //e.g. GIF 1 at x = 0, GIF 2 at x = 360, GIF 3 at x = 720, etc.
+            let xPosition = view.frame.width * index
+            
+            //set the image view to reside at that xPosition, and to fit the screen
+            imageView.frame = CGRect(x: xPosition, y: 0, width: scrollView.frame.width, height: scrollView.frame.height)
+            
+            //make the content view large enough to fit all of the GIFs
+            scrollView.contentSize.width = scrollView.frame.width * (index + 1)
+            //add the GIF to the scroll view so we can see it
+            scrollView.addSubview(imageView)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
